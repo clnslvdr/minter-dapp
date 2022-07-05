1252e2d2200b6f8cf11cc20f55e2edecab8f210f
coleen salvador <coleensalvador@granddudestudios.com>
2022-07-05 16:03:52 +0800

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -13,7 +13,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 100,
+    growEditionSizeTo: 50,
     layersOrder: [
       { name: "Backgrounds" },
       { name: "Body" },
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-02-08T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xc1872b38Ce02A0060748b7acFd7A8182B2b9aaD1"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
