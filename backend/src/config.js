1281f7d39d40b0152b44828593d127678d7a8074
coleen salvador <coleensalvador@granddudestudios.com>
2022-07-07 07:15:02 +0800

    mint address

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xc1872b38Ce02A0060748b7acFd7A8182B2b9aaD1"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xc1872b38Ce02A0060748b7acFd7A8182B2b9aaD1"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
